08bb6a66da626ab8d00d59a674243d9c2a335d54
Nevena Kolev <nevena.kolev@htl-wels.at>
2021-01-21 11:59:41 +0100

    added classes ColorCode.java and ModularCounter.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,8 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**************************
+ * @author Nevena Kolev
+ * @version 1.0, 21-01-21
+ **************************/
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -18,6 +21,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        //launches the JavaFX Application
         launch(args);
     }
 }
